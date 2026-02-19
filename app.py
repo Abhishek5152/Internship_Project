07c99dcb291714141dbffe7837cc07499076ef2b
Abhishek <abhishekbhojaviya@gmail.com>
2026-02-19 15:29:28 +0530

    Added manage users option for managers limited to employees

--- app.py
+++ app.py
@@ -485,6 +485,7 @@ def add_log(user_id, action_type, entity_type, entity_id, description):
         cursor.close()
 
 @app.route('/viewlogs')
+@login_required
 def view_logs():
     cursor = conn.cursor()
     try:
@@ -616,10 +617,10 @@ def userlogin():
 
             msg = "Login successful!"
 
-            if user[4] == 'Manager':
+            if user[4] == 'Manager' and user[5] == 'Active':
                 session["user_role"] = 'Manager'
                 return redirect(url_for('mandash', msg=msg))
-            elif user[4] == 'Employee':
+            elif user[4] == 'Employee' and user[5] == 'Active':
                 session["user_role"] = 'Employee'
                 return redirect(url_for('empdash', msg=msg))
             elif user[4] == 'Admin':
@@ -638,6 +639,58 @@ def userlogin():
 @login_required
 def mandash():
     return render_template('manager/man_dashboard.html')
+
+@app.route('/manusers-m')
+@login_required
+def manusers_m():
+    cursor = conn.cursor()
+    try:
+        cursor.execute("SELECT * FROM eerm_users where user_role = 'Employee'")
+        users = cursor.fetchall()
+        return render_template('manager/man_manusers.html', users=users)
+    except Exception as e:
+        print("Error fetching users:", e)
+        return "Error fetching users"
+    finally:
+        cursor.close()
+
+@app.route('/toggle_user_status/<int:user_id>', methods=['POST'])
+@login_required
+def toggle_user_status(user_id):
+    cursor = conn.cursor()
+    try:
+        cursor.execute("SELECT user_status FROM eerm_users WHERE user_id = %s", (user_id,))
+        result = cursor.fetchone()
+
+        if result is None:
+            return "User not found"
+
+        current_status = result[0]
+        new_status = 'Inactive' if current_status == 'Active' else 'Active'
+
+        if new_status == 'Inactive':
+            cursor.execute("UPDATE eerm_users SET user_status = 'Inactive' WHERE user_id = %s", (user_id,))
+        else:
+            cursor.execute("UPDATE eerm_users SET user_status = 'Active' WHERE user_id = %s", (user_id,))
+        conn.commit()
+
+        add_log(
+            session.get("admin_id"),
+            "TOGGLE",
+            "USER",
+            user_id,
+            f"Changed user status to {new_status}"
+        )
+
+
+        return redirect(url_for('manusers_m'))
+
+    except Exception as e:
+        print("Error toggling user status:", e)
+        return "Error toggling user status"
+
+    finally:
+        cursor.close()
 
 
 if __name__ == '__main__':
