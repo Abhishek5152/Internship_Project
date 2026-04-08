24a030d200f88eb85c42a562aea611e0a1de93d9
Abhishek <abhishekbhojaviya@gmail.com>
2026-04-08 15:48:06 +0530

    Changes in database.py and app.py to use .env

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ from blueprints.auth import auth_bp
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
--- database.py
+++ database.py
@@ -1,13 +1,17 @@
 import pymysql
 from flask import g
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def get_db_connection():
     if 'db_connection' not in g:
         g.db_connection = pymysql.connect(
-            host='localhost',
-            user='root',
-            password='',
-            database='db_eerm',
+            host=os.getenv('DB_HOST'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            database=os.getenv('DB_NAME'),
             autocommit=False
         )
     return g.db_connection
